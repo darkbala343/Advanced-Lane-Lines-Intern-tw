7d5ad6e526a3b91fb6a05121af2c18e1b9dd36b3
darkbala343 <118290899+darkbala343@users.noreply.github.com>
2023-12-05 08:17:36 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,8 @@
-"""
-Lane Lines Detection pipeline
-
-Usage:
-    main.py [--video] INPUT_PATH OUTPUT_PATH 
-
-Options:
-
--h --help                               show this screen
---video                                 process video file instead of image
-"""
-
 import numpy as np
 import matplotlib.image as mpimg
 import cv2
-from docopt import docopt
-from IPython.display import HTML, Video
+from IPython.display import HTML
+from IPython.core.display import Video
 from moviepy.editor import VideoFileClip
 from CameraCalibration import CameraCalibration
 from Thresholding import *
@@ -22,11 +10,6 @@ from PerspectiveTransformation import *
 from LaneLines import *
 
 class FindLaneLines:
-    """ This class is for parameter tunning.
-
-    Attributes:
-        ...
-    """
     def __init__(self):
         """ Init Application"""
         self.calibration = CameraCalibration('camera_cal', 9, 6)
@@ -57,16 +40,8 @@ class FindLaneLines:
         out_clip.write_videofile(output_path, audio=False)
 
 def main():
-    args = docopt(__doc__)
-    input = args['INPUT_PATH']
-    output = args['OUTPUT_PATH']
-
     findLaneLines = FindLaneLines()
-    if args['--video']:
-        findLaneLines.process_video(input, output)
-    else:
-        findLaneLines.process_image(input, output)
-
+    findLaneLines.process_video("challenge_video.mp4","output.mp4")
 
 if __name__ == "__main__":
     main()
